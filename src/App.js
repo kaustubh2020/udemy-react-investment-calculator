96281294d4d0ae2a7b37f2036af0ebe9bc66a19b
kaustubh2020 <kaustubhjaiswal200@gamil.com>
2023-10-11 23:37:14 +0530

    Output results

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ function App() {
     setUserInput(userInput);
   };
 
-  if (userInput) {
-    const yearlyData = [];
+  const yearlyData = [];
 
+  if (userInput) {
     let currentSavings = +userInput["current-savings"];
     const yearlyContribution = +userInput["yearly-contribution"];
     const expectedReturn = +userInput["expected-return"] / 100;
@@ -34,7 +34,13 @@ function App() {
     <div>
       <Header title={"Investment Calculator"} />
       <UserInput onCalculate={calculateHandler} />
-      <ResultsTable />
+      {!userInput && <p>No Investment Calculated.</p>}
+      {userInput && (
+        <ResultsTable
+          data={yearlyData}
+          initialInvestment={userInput["current-savings"]}
+        />
+      )}
     </div>
   );
 }
